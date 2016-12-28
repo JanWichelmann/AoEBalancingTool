73471efc426cb70547c224799e313bab5bb68129
Janworks <jan.wichelmann@hotmail.de>
2016-12-28 16:34:48 +0100

    Initial commit

---
+++ AoEBalancingTool/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AoEBalancingTool
+{
+	/// <summary>
+	/// Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		#region Variables
+
+		#endregion
+
+		#region Functions
+
+		public MainWindow()
+		{
+			// Initialize controls
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Event handlers
+
+		private void _loadFileButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		#endregion
+	}
+}
